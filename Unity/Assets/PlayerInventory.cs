63c9c0d11124aa72a631525742561ebeead31454
Marcos Lucas <42158901+marcosllbs@users.noreply.github.com>
2022-04-07 23:02:38 -0300

    ForestScapeBuild1.01

---
+++ Unity/Assets/PlayerInventory.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerInventory : MonoBehaviour
+{
+       public int NumberOfDiamonds { get; private set;}
+
+    public UnityEvent<PlayerInventory> OnDiamondCollected;
+    public void DiamondCollected()
+
+    {
+        NumberOfDiamonds++;
+        OnDiamondCollected.Invoke(this);
+    }
+}
